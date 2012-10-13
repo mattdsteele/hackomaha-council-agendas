589cb44f2e57a30f3c01eca1018e1c509b964efe
Matt Steele <orphum@gmail.com>
2012-10-13 14:37:03 -0500

    Clean up NameFinder code a bit

--- nlp/src/hackomaha/NameFinder.java
+++ nlp/src/hackomaha/NameFinder.java
@@ -8,27 +8,35 @@ import java.util.*;
 
 public class NameFinder {
     private final NameFinderME nameFinder;
+    private SentenceDetector sentenceDetector;
 
     public NameFinder() {
         TokenNameFinderModel model = Builder.tokenNameFinderModel("resources/en-ner-person.bin");
         nameFinder = new NameFinderME(model);
+        sentenceDetector = new SentenceDetector();
     }
 
     public Set<String> findNames(String phrase) throws Exception {
         HashSet<String> names = new HashSet<String>();
-        String[] sentences = new SentenceDetector().getSentences(phrase);
-        for (String sentence : sentences) {
-            String[] words = sentence.split(" ");
-            Span[] spans = nameFinder.find(words);
-            for (Span span : spans) {
-                String val = "";
-                for (int i = span.getStart(); i < span.getEnd(); i++) {
-                    val += words[i] + " " ;
-                }
-                names.add(val.trim());
-            }
-
+        for (String sentence : sentenceDetector.getSentences(phrase)) {
+            handleSentence(names, sentence);
         }
         return names;
+    }
+
+    private void handleSentence(HashSet<String> names, String sentence) {
+        String[] words = sentence.split(" ");
+        Span[] spans = nameFinder.find(words);
+        for (Span span : spans) {
+            names.add(addPhrase(words, span));
+        }
+    }
+
+    private String addPhrase(String[] words, Span span) {
+        String val = "";
+        for (int i = span.getStart(); i < span.getEnd(); i++) {
+            val += words[i] + " " ;
+        }
+        return val.trim();
     }
 }
