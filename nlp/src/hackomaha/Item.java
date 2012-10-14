8f1662bad2c9ee5ad8f85ee6dde79131440a26bd
corey jergensen <corey.jergensen@gmail.com>
2012-10-13 19:21:25 -0500

    add item processing.

--- nlp/src/hackomaha/Item.java
+++ nlp/src/hackomaha/Item.java
@@ -1,5 +1,6 @@
 package hackomaha;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Item {
@@ -10,5 +11,22 @@ public class Item {
 	}
 	
 	public void process() {
+		try {
+//			for (int i = 0; i < json.getJSONArray("text").length(); i++) {
+//				String currItemStr = (String)json.getJSONArray("text").get(i);
+//				AgendaItem agendaItem = new AgendaItem(currItemStr);
+//				
+//			}
+			String allItems = "";
+			for (int i = 0; i < json.getJSONArray("text").length(); i++) {
+				allItems += json.getJSONArray("text").get(i) + "\n";
+			}
+			AgendaItem agendaItem = new AgendaItem(allItems);
+			for(String currKey : agendaItem.map.keySet()) {
+				this.json.put(currKey, agendaItem.map.get(currKey));
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 	}
 }
